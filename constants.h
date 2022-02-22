8acf979eed0c47b3e2e2e3497ca1b8833c8d1f15
David Zhuravlev <david99111@mial.ru>
2022-02-22 12:03:32 +0300

    removed_for_loops

--- constants.h
+++ constants.h
@@ -4,11 +4,11 @@
 # define RES_X		854
 # define RES_Y		480
 # define TILE		128
-# define MINI_MAP_W	0.3
+# define MINI_MAP_W	0.5
 # define PL_HEIGHT	64
 # define STEP_SIZE	0.3f
 # define PI			3.141592653
 # define DEG		PI / 180.0f
-# define FOV		PI / 2.0f
+# define FOV		PI / 2
 
 #endif
--- render/minimap.c
+++ render/minimap.c
@@ -2,32 +2,43 @@
 
 void	render_rays(t_state *state, t_img *img, t_vec **rays, float scale)
 {
-	float size = (RES_X * scale) / state->map->width;
+	float size;
+	size_t	i;
 	t_vec	vec;
 
-	for (int i = 0; i < RES_X - 1; i += 10)
+	i = 0;
+	size = (RES_X * scale) / state->map->width;
+	while (i < RES_X - 1)
 	{
 		vec.x = rays[i]->x * size;
 		vec.y = rays[i]->y * size;
 		put_pixel(img, (int)(vec.x), (int)(vec.y), COL_YELLOW);
+		i += 10;
 	}
 }
 
 void	render_player(t_state *state, t_img *img, float scale)
 {
-	size_t	pl_size = state->size / 3;
+	size_t	pl_size;
+	size_t	i;
+	size_t	j;
 	int		dx;
 	int		dy;
 
-	for (size_t i = state->pl.x - pl_size; i < state->pl.x + pl_size; i++)
+	pl_size = state->size / 3;
+	i = state->pl.x - pl_size;
+	while (i < state->pl.x + pl_size)
 	{
-		for (size_t j = state->pl.y - pl_size; j < state->pl.y + pl_size; j++)
+		j = state->pl.y - pl_size;
+		while (j < state->pl.y + pl_size)
 		{
 			dx = i - state->pl.x;
 			dy = j - state->pl.y;
 			if (dx * dx + dy * dy < pl_size * pl_size)
 				put_pixel(img, i * scale, j  * scale, COL_GREEN);
+			j++;
 		}
+		i++;
 	}
 	put_pixel(img, state->pl.x * scale, state->pl.y * scale, COL_RED);
 }
@@ -38,10 +49,10 @@ void	draw_rec(t_img *img, t_rec *rec)
 	size_t	j;
 
 	i = rec->x;
-	while (i < rec->width)
+	while (i < rec->width + 1)
 	{
 		j = rec->y;
-		while (j < rec->height)
+		while (j < rec->height + 1)
 		{
 			put_pixel(img, i, j, rec->color);
 			j++;
@@ -58,7 +69,6 @@ float	calc_scale(t_state *state)
 	max_width = (int)floorf(RES_X * MINI_MAP_W);
 	while (max_width % state->map->width != 0)
 		max_width--;
-	printf("width %d\n", max_width);
 	return ((float)max_width / (float)RES_X);
 }
 
@@ -71,7 +81,6 @@ void	render_map(t_state *state, t_img *img, t_vec **rays)
 
 	i = 0;
 	scale = calc_scale(state);
-	printf("%f\n", scale);
 	while (i < state->map->height)
 	{
 		j = 0;
