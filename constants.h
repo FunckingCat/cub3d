baf8b8f518f81c154d0ae1c841101fc2e0c6ed66
Toad Yamcha <tyamcha@mi-h2.msk.21-school.ru>
2022-03-14 15:23:18 +0300

    new

--- constants.h
+++ constants.h
@@ -6,18 +6,18 @@
 /*   By: tyamcha <tyamcha@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/03 17:00:02 by tyamcha           #+#    #+#             */
-/*   Updated: 2022/03/03 17:21:35 by tyamcha          ###   ########.fr       */
+/*   Updated: 2022/03/14 15:20:20 by tyamcha          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CONSTANTS_H
 # define CONSTANTS_H
 
-# define RES_X		854
-# define RES_Y		480
-# define MINI_MAP_W	0.5
+# define RES_X		1280
+# define RES_Y		720
+# define MINI_MAP_W	0.4
 # define STEP_SIZE	0.25f
 # define PI			3.141592653
-# define DEG		0.0174533
+# define DEG		0.01
 
 #endif
